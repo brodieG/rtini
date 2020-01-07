5cf7294f966dde60f0b18d98003747cc9a718c7c
brodieG <brodieG@users.noreply.github.com>
2020-01-06 20:18:39 -0500

    copyright notice

--- R/extract.R
+++ R/extract.R
@@ -1,3 +1,20 @@
+## Copyright (C) 2020  Brodie Gaslam
+##
+## This file is part of "rtini - Right Triangle Irregular Network Surface
+## Approximation"
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## Go to <https://www.r-project.org/Licenses/GPL-2> for a copy of the license.
+
 #' Extract A Triangle Mesh With Approximation Error Below Tolerance
 #'
 #' Given a surface approximation error matrix as computed with [rtini_error()],
